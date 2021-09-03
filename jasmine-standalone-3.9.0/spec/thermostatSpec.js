928662e92f900e6e5a6c5cde493ca0caa6cfc7c6
Suhani Zaman <suhanizaman15@gmail.com>
2021-09-03 18:37:25 +0400

    thermostat min temperature added

--- jasmine-standalone-3.9.0/spec/thermostatSpec.js
+++ jasmine-standalone-3.9.0/spec/thermostatSpec.js
@@ -7,18 +7,24 @@ describe('Thermostat', function(){
     it("checks function thermostat", function() {
       expect(thermostat.temperature).toBeDefined();
     });
-    it("cheks tempearture is 20 degree", function(){
+    it("cheks tempearture is 20 degree at start", function(){
       expect(thermostat.temperature()).toEqual(20)
     });
   });
   describe("increase and descrease:", function(){
     it ("increases the temperature", function(){
-      thermostat.up(5)
-      expect(thermostat.temperature()).toEqual(25)
+      thermostat.up()
+      expect(thermostat.temperature()).toEqual(21)
     });
     it("decreases the temperature", function(){
-      thermostat.down(3)
-      expect(thermostat.temperature()).toEqual(17)
+      thermostat.down()
+      expect(thermostat.temperature()).toEqual(19)
+    });
+    it("checks minimum temperature is 10", function(){
+      for(let i = 0; i<12; i++){
+        thermostat.down() 
+      }
+      expect(thermostat.temperature()).toEqual(10)
     });
   });
-});
+}); 
--- jasmine-standalone-3.9.0/src/thermostat.js
+++ jasmine-standalone-3.9.0/src/thermostat.js
@@ -1,14 +1,19 @@
 class Thermostat {
   constructor(){
     this.temp = 20
+    this.MIN_TEMP = 10
   }
   temperature(){
     return this.temp;
   }
-  up(number){
-    this.temp += number;
+  up(){
+    this.temp++;
   }
-  down(number){
-    this.temp -= number;
+  down(){
+    if (this.temp > 10){
+      this.temp--;
+    }else{
+      return this.MIN_TEMP
+    }
   }
 }
